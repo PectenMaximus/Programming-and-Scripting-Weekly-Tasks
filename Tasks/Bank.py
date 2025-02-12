a3a231c5d28e242a674d2685679860308d35c384
Alec Reid <muckrossreid@gmail.com>
2025-02-12 15:55:50 +0000

    Task 2 - Completed (Code and Notebook)

---
+++ Tasks/Bank.py
@@ -0,0 +1,6 @@
+# Enter an amouunt of money in cents
+Amount_1 = int(input("Enter an amount of money in cents: "))
+Amount_2 = int(input("Enter an amount of money in cents: "))
+Total = float(Amount_1 + Amount_2)
+print ("The total amount of money is: €",  Total/100)
+
