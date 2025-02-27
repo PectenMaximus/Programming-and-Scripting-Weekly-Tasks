62f831dda4626500958d63cace6587a2c62c5534
Alec Reid <muckrossreid@gmail.com>
2025-02-27 17:34:34 +0000

    Fixed Lab 4.2
    1. Added a missing colon (`:`) after `if guess > number_to_guess`.
    2. Removed the redundant `else` block.
    3. Moved the `guess = int(input("Guess again: "))` line outside of the `if` and `elif` blocks to ensure it only prompts for a new guess once per iteration.

--- Lab 4.2/guess2.py
+++ Lab 4.2/guess2.py
@@ -3,13 +3,13 @@
 import numpy as np
 number_to_guess = np.random.randint(1, 20)
 
-guess = int (input ('Please guess a number: '))
+guess = int(input('Please guess a number: '))
 
 while guess != number_to_guess:
     if guess < number_to_guess:
-        print ("Too Low!")
-    else:
-        print ("Too High!")
-        guess = int (input("guess again:"))
+        print("Too Low!")
+    elif guess > number_to_guess:
+        print("Too High!")
+    guess = int(input("Guess again: "))
 
-print ('well done! it was',number_to_guess)
+print('Well done! It was', number_to_guess)
