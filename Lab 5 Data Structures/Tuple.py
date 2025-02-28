1467b2b619f759ba48521462ddf38563c2c697ae
Alec Reid <muckrossreid@gmail.com>
2025-02-28 17:01:04 +0000

    Lab 5.1 - Tuple.py

---
+++ Lab 5 Data Structures/Tuple.py
@@ -0,0 +1,8 @@
+# Create a tuple that stores the months of the year, from that tupple create another tuple with just the summer months
+
+months_of_year = ("Jan","Feb","Mar","April","May","June","July","August","September","October","November","December")
+
+summer_months = months_of_year [4:9]
+for month in summer_months:
+    print(summer_months)
+
